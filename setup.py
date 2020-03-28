00ba329a8fd3549f1da6d6ce4f52d11a2763b017
Loris Cro <kappaloris@gmail.com>
2020-03-28 04:20:16 +0100

    docs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="redismpx", # Replace with your own username
-    version="0.0.2",
+    version="0.1.0",
     author="Loris Cro",
     author_email="kappaloris@gmail.com",
     description="A Redis Pub/Sub multiplexer.",
